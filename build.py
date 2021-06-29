3ebfab9c8105229b7a1cbe7f417c241421390aa7
ousttrue <ousttrue@gmail.com>
2021-06-30 02:13:36 +0900

    update

--- build.py
+++ build.py
@@ -2,6 +2,7 @@
 import subprocess
 import pathlib
 import os
+import platform
 from typing import List
 
 HERE = pathlib.Path(__file__).parent
@@ -10,7 +11,12 @@ DEPS = NEOVIM_DIR / '.deps'
 BUILD = NEOVIM_DIR / 'build'
 
 
-def run(cmd: List[str]):
+def install_packages(*packages: List[str]):
+    if platform.system() == 'Linux':
+        run(['sudo', 'apt', 'install', '-y'] + packages)
+
+
+def run(*cmd: List[str]):
     print(' '.join(cmd))
     process = subprocess.Popen(cmd, stdout=subprocess.PIPE)
     while True:
@@ -22,22 +28,36 @@ def run(cmd: List[str]):
     print(rc)
 
 
+def install():
+    if platform.system() == 'Linux':
+        # install to /usr/local/bin
+        run('sudo', 'cmake', '--install', '.')
+    else:
+        raise NotImplementedError()
+
+
 if __name__ == '__main__':
 
-    run(['sudo', 'apt', 'install', 'libtool-bin', 'cmake', '-y'])
+    install_packages('libtool-bin', 'cmake')
 
     # https://github.com/neovim/neovim/wiki/Building-Neovim
 
+    #
+    # build libs
+    #
     if not DEPS.exists():
         DEPS.mkdir()
     os.chdir(DEPS)
-    run(['cmake', '../third-party'])
-    run(['cmake', '--build', '.'])
+    run('cmake', '../third-party')
+    run('cmake', '--build', '.')
 
+    #
+    # build nvim
+    #
     if not BUILD.exists():
         BUILD.mkdir()
     os.chdir(BUILD)
-    run(['cmake', '..'])
-    run(['cmake', '--build', '.'])
-    # install to /usr/local/bin
-    run(['sudo', 'cmake', '--install', '.'])
+    run('cmake', '..')
+    run('cmake', '--build', '.')
+
+    install()
