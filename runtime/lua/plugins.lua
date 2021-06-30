90916407f5796c240f7f2455e3387b4a54445c56
ousttrue <ousttrue@gmail.com>
2021-07-01 02:57:13 +0900

    update

--- runtime/lua/plugins.lua
+++ runtime/lua/plugins.lua
@@ -2,9 +2,12 @@ vim.cmd([[packadd packer.nvim]])
 
 local packer = require("packer")
 packer.startup(function()
-	-- 起動時に読み込むプラグインは名前を書くだけです
+	use("arcticicestudio/nord-vim")
+	use("itchyny/lightline.vim")
+
+	use("wbthomason/packer.nvim")
 	use("tpope/vim-fugitive")
-	use("tpope/vim-repeat")
+	use("rhysd/git-messenger.vim")
 
 	use({
 		"nvim-telescope/telescope.nvim",
@@ -12,15 +15,6 @@ packer.startup(function()
 	})
 
 	use("kyazdani42/nvim-tree.lua")
-
-	-- opt オプションを付けると遅延読み込みになります。
-	-- この場合は opt だけで読み込む契機を指定していないため、
-	-- `packadd` コマンドを叩かない限り読み込まれることはありません。
-	-- use({ "wbthomason/packer.nvim", opt = true })
-	-- packer.nvim 自体を遅延読み込みにする理由はまた後ほど。
-
-	-- コマンドを叩いたときに読み込む。
-	use({ "rhysd/git-messenger.vim", opt = true, cmd = { "GitMessenger" } })
 
 	-- 特定のイベントで読み込む
 	use({ "tpope/vim-unimpaired", opt = true, event = { "FocusLost", "CursorHold" } })
--- runtime/lua/setup.lua
+++ runtime/lua/setup.lua
@@ -1,9 +1,20 @@
 -- settings
+vim.cmd[[set termguicolors]]
 
 -- keymaps
 vim.cmd[[nmap <C-n> :lnext<CR>]]
 vim.cmd[[nmap <C-p> :lprevious<CR>]]
 vim.cmd[[nnoremap <C-l> :nohlsearch<CR><C-l>]]
 vim.cmd[[nnoremap q :close<CR> ]]
+
+vim.cmd[[nmap <C-_> :Commentary<CR>]]
+vim.cmd[[vmap <C-_> :Commentary<CR>]]
+vim.cmd[[nmap <C-/> :Commentary<CR>]]
+vim.cmd[[vmap <C-/> :Commentary<CR>]]
+
 vim.cmd[[nmap <silent> <Space><Space> :<C-u>Telescope<CR>]]
 vim.cmd[[nmap <silent> ,, :<C-u>NvimTreeToggle<CR>]]
+
+-- colorscheme
+vim.cmd[[colorscheme nord]]
+
