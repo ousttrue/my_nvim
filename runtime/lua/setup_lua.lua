bfdc1fffc72592ec6cb0971d02df21f5507595ca
ousttrue <ousttrue@gmail.com>
2021-07-04 01:44:33 +0900

    lsp_on_attach

--- runtime/lua/setup_lua.lua
+++ runtime/lua/setup_lua.lua
@@ -1,19 +1,19 @@
 --
 -- lua
 --
-local custom_lsp_attach = function(client)
-	-- See `:help nvim_buf_set_keymap()` for more information
-	vim.api.nvim_buf_set_keymap(0, "n", "K", "<cmd>lua vim.lsp.buf.hover()<CR>", { noremap = true })
-	vim.api.nvim_buf_set_keymap(0, "n", "<c-]>", "<cmd>lua vim.lsp.buf.definition()<CR>", { noremap = true })
-	-- ... and other keymappings for LSP
+-- local custom_lsp_attach = function(client)
+-- 	-- See `:help nvim_buf_set_keymap()` for more information
+-- 	vim.api.nvim_buf_set_keymap(0, "n", "K", "<cmd>lua vim.lsp.buf.hover()<CR>", { noremap = true })
+-- 	vim.api.nvim_buf_set_keymap(0, "n", "<c-]>", "<cmd>lua vim.lsp.buf.definition()<CR>", { noremap = true })
+-- 	-- ... and other keymappings for LSP
 
-	-- Use LSP as the handler for omnifunc.
-	--    See `:help omnifunc` and `:help ins-completion` for more information.
-	vim.api.nvim_buf_set_option(0, "omnifunc", "v:lua.vim.lsp.omnifunc")
+-- 	-- Use LSP as the handler for omnifunc.
+-- 	--    See `:help omnifunc` and `:help ins-completion` for more information.
+-- 	vim.api.nvim_buf_set_option(0, "omnifunc", "v:lua.vim.lsp.omnifunc")
 
-	-- For plugins with an `on_attach` callback, call them here. For example:
-	-- require('completion').on_attach()
-end
+-- 	-- For plugins with an `on_attach` callback, call them here. For example:
+-- 	-- require('completion').on_attach()
+-- end
 
 local sumneko_root_path = vim.api.nvim_get_var("my_nvim_root") .. "\\language_server\\lua-language-server"
 local sumneko_binary = sumneko_root_path .. "\\bin\\Windows\\lua-language-server.exe"
@@ -41,5 +41,5 @@ require("lspconfig").sumneko_lua.setup({
 			},
 		},
 	},
-	on_attach = custom_lsp_attach,
+	on_attach = require("lsp_on_attach"),
 })
--- runtime/lua/setup_python.lua
+++ runtime/lua/setup_python.lua
@@ -1,9 +1,11 @@
-require("lspconfig").pylsp.setup({})
+require("lspconfig").pylsp.setup({
+	on_attach = require("lsp_on_attach"),
+})
 
 local dap = require("dap")
 dap.adapters.python = {
 	type = "executable",
-	command = vim.api.nvim_get_var('python3_host_prog'),
+	command = vim.api.nvim_get_var("python3_host_prog"),
 	args = { "-m", "debugpy.adapter" },
 }
 dap.configurations.python = {
@@ -13,8 +15,7 @@ dap.configurations.python = {
 		name = "Launch file",
 		program = "${file}",
 		pythonPath = function()
-			return vim.api.nvim_get_var('python3_host_prog')
+			return vim.api.nvim_get_var("python3_host_prog")
 		end,
 	},
 }
-
