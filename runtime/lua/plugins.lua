fa47bb03198c31da173b9235cb4353111483683b
ousttrue <ousttrue@gmail.com>
2021-07-04 19:56:50 +0900

    symbols-outline

--- runtime/lua/plugins.lua
+++ runtime/lua/plugins.lua
@@ -73,6 +73,29 @@ packer.startup(function()
 		end,
 	})
 	use("nvim-treesitter/playground")
+	use({
+		"simrat39/symbols-outline.nvim",
+		config = function()
+			vim.g.symbols_outline = {
+				highlight_hovered_item = true,
+				show_guides = false,
+				auto_preview = false,
+				position = "left",
+				show_numbers = false,
+				show_relative_numbers = false,
+				show_symbol_details = true,
+				keymaps = {
+					close = "<Esc>",
+					goto_location = "<Cr>",
+					focus_location = "o",
+					hover_symbol = "<C-space>",
+					rename_symbol = "r",
+					code_actions = "a",
+				},
+				lsp_blacklist = {},
+			}
+		end,
+	})
 
 	-- git
 	use("tpope/vim-fugitive")
