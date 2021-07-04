c696f20c02f02640d8aea203350eed32e1cbd5d2
ousttrue <ousttrue@gmail.com>
2021-07-04 17:00:07 +0900

    lspkind

--- runtime/lua/plugins.lua
+++ runtime/lua/plugins.lua
@@ -148,6 +148,50 @@ packer.startup(function()
 	-- 	end,
 	-- })
 	use({
+		"onsails/lspkind-nvim",
+		config = function()
+			require("lspkind").init({
+				-- enables text annotations
+				--
+				-- default: true
+				with_text = true,
+
+				-- default symbol map
+				-- can be either 'default' or
+				-- 'codicons' for codicon preset (requires vscode-codicons font installed)
+				--
+				-- default: 'default'
+				preset = "codicons",
+
+				-- override preset symbols
+				--
+				-- default: {}
+				symbol_map = {
+					Text = "",
+					Method = "ƒ",
+					Function = "",
+					Constructor = "",
+					Variable = "",
+					Class = "",
+					Interface = "ﰮ",
+					Module = "",
+					Property = "",
+					Unit = "",
+					Value = "",
+					Enum = "了",
+					Keyword = "",
+					Snippet = "﬌",
+					Color = "",
+					File = "",
+					Folder = "",
+					EnumMember = "",
+					Constant = "",
+					Struct = "",
+				},
+			})
+		end,
+	})
+	use({
 		"hrsh7th/nvim-compe",
 		config = function()
 			require("compe").setup({
