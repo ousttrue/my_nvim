88a4213966ade6544d243c92afb9068134dc0f1a
ousttrue <ousttrue@gmail.com>
2021-11-07 21:30:51 +0900

    plugins/use_core.lua

--- runtime/lua/noplugins.lua
+++ runtime/lua/noplugins.lua
@@ -7,56 +7,12 @@ require("packer.luarocks").install_commands()
 -- require('luarocks.loader')
 
 packer.startup(function()
-    -- pckage
     -- https://github.com/wbthomason/packer.nvim
     use "nvim-lua/plenary.nvim"
     use "wbthomason/packer.nvim"
 
-    use "Shougo/context_filetype.vim"
-    use "osyo-manga/vim-precious"
-
-    use "cespare/vim-toml"
-    use "tpope/vim-fugitive"
-    use {
-        "tpope/vim-commentary",
-        config = function()
-            vim.api.nvim_set_keymap("n", "<C-_>", ":Commentary<CR>", { noremap = true })
-            vim.api.nvim_set_keymap("v", "<C-_>", ":Commentary<CR>", { noremap = true })
-        end,
-    }
-
-    -- use "itchyny/lightline.vim"
-    -- use "adelarsq/neoline.vim"
-    use {
-        "nvim-lualine/lualine.nvim",
-        requires = { "kyazdani42/nvim-web-devicons", opt = true },
-        config = function()
-            require("lualine").setup()
-        end,
-    }
-    use {
-        "Chiel92/vim-autoformat",
-        config = function()
-            vim.api.nvim_set_keymap("n", "<S-f>", ":Autoformat<CR>", { noremap = true })
-            vim.api.nvim_set_keymap("v", "<S-f>", ":Autoformat<CR>", { noremap = true })
-        end,
-    }
-
-    use {
-        "preservim/nerdtree",
-        config = function()
-            vim.api.nvim_set_keymap("n", ",,", ":NERDTreeFind<CR>", { noremap = true })
-        end,
-    }
-
-    use "nathanaelkane/vim-indent-guides"
-
-    use {
-        "simeji/winresizer",
-        config = function()
-            -- vim.api.nvim_set_var("winresizer_start_key", "<C-T>")
-        end,
-    }
+    -- pckage
+    require "plugins.use_core"(use)
 end)
 
 vim.cmd "PackerInstall"
