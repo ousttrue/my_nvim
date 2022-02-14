b1d480961cada53ef67a28a2f6741d678225e5e6
ousttrue <ousttrue@gmail.com>
2022-02-14 16:34:43 +0900

    update

--- runtime/lua/noplugins.lua
+++ runtime/lua/noplugins.lua
@@ -1,25 +1,56 @@
---
--- https://oroques.dev/notes/neovim-init/
---
-local packer = require "packer"
-require("packer.luarocks").install_commands()
--- require('packer.luarocks').setup_paths()
--- require('luarocks.loader')
+local fn = vim.fn
 
-packer.startup(function()
-    -- https://github.com/wbthomason/packer.nvim
-    use "nvim-lua/plenary.nvim"
-    use "wbthomason/packer.nvim"
+-- Automatically install packer
+local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
+if fn.empty(fn.glob(install_path)) > 0 then
+  PACKER_BOOTSTRAP = fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
+  print("Installing packer close and reopen Neovim...")
+end
 
-    -- package
-    require "plugins.use_core"(use)
-    require "plugins.use_treesitter"(use)
-    require "plugins.use_lsp"(use)
-    require "plugins.use_dap"(use)
-    require "plugins.use_skk"(use)
+-- Autocommand that reloads neovim whenever you save the plugins.lua file
+vim.cmd([[
+  augroup packer_user_config
+    autocmd!
+    autocmd BufWritePost plugins.lua source <afile> | PackerSync
+  augroup end
+]])
 
-    use "neoclide/jsonc.vim"
+-- Use a protected call so we don't error out on first use
+local status_ok, packer = pcall(require, "packer")
+if not status_ok then
+  return
+end
+
+-- Have packer use a popup window
+packer.init({
+    display = {
+      open_fn = function()
+        return require('packer.util').float({ border = 'single' })
+      end
+    }
+  }
+)
+
+-- Install your plugins here
+return packer.startup(function(use)
+  -- My plugins here
+  use 'wbthomason/packer.nvim' -- Have packer manage itself
+  use "nvim-lua/popup.nvim" -- An implementation of the Popup API from vim in Neovim
+  use "nvim-lua/plenary.nvim" -- Useful lua functions used ny lots of plugins
+
+  -- package
+  require "plugins.use_core"(use)
+  require "plugins.use_treesitter"(use)
+  require "plugins.use_lsp"(use)
+  require "plugins.use_dap"(use)
+  require "plugins.use_skk"(use)
+
+  use "neoclide/jsonc.vim"
+
+  -- Automatically set up your configuration after cloning packer.nvim
+  -- Put this at the end after all plugins
+  if PACKER_BOOTSTRAP then
+    require('packer').sync()
+  end
 end)
-
-vim.cmd "PackerInstall"
 
